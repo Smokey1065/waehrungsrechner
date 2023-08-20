d23d6e764ff6afc235c7dd056112454da60dface
Smokey1065 <hessorogan@gmail.com>
2023-08-21 01:25:25 +0200

    Big Update

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,13 +1,26 @@
-function berechnen(){
-    let euro= euroFeld.value;
-    let result = euro * 1.0942;
-    usd.value = result.toFixed(2).replace('.', ',');
+function berechnen() {
+  let euro = euroFeld.value;
+  let result = euro * 1.0942;
+  usd.value = result.toFixed(2).replace(".", ",");
 }
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// Warte bis die Seite geladen ist
+window.addEventListener("load", function () {
+  // Verstecke den Preloader
+  document.querySelector(".preloader").style.display = "none";
+  // Zeige den eigentlichen Inhalt
+  document.querySelector(".content").style.display = "block";
+});
+
+
+
+
+
