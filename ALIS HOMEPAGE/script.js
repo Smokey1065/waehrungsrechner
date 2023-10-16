57aa8f9311c6464c321606eff6492ac43f064d26
Smokey1065 <95089053+Smokey1065@users.noreply.github.com>
2023-10-16 08:20:15 +0200

    Add files via upload

---
+++ ALIS HOMEPAGE/script.js
@@ -0,0 +1,21 @@
+
+
+
+function dm() {
+
+    window.location.href='https://taschenrechner.netlify.app/'
+
+
+}
+
+
+function dp () {
+
+    window.location.href='https://waehrungrechner.netlify.app/';
+} 
+
+
+
+document.getElementById('coolButton').addEventListener('click', function() {
+    document.getElementById('result').innerText = 'Wow, das ist wirklich cool!';
+});
